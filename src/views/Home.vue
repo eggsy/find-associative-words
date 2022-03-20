e17c77f398bce0a6e78a66c7c21317e9470113b7
Abdulbaki Dursun <eggsydev@gmail.com>
2022-03-20 16:49:34 +0300

    chore: change strings on the main page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="flex flex-col text-center gap-y-4">
-    <h1 class="text-6xl font-semibold">Find Common Words</h1>
+    <h1 class="text-6xl font-semibold">Find Associative Words</h1>
 
     <p class="text-xl opacity-50 md:w-2/3 md:mx-auto">
-      An interactive app created by the presenter to play
-      <span class="font-bold underline">Find the Associative Meanings</span>
-      of a random word!
+      An interactive app created by the presenter to play "find associative
+      words" of a random word! Check the rules
+      <a
+        href="https://github.com/eggsy/find-associative-words#rules--how-to-play"
+        target="_blank"
+        rel="noreferrer"
+        class="underline"
+        >here</a
+      >!
     </p>
 
     <router-link
